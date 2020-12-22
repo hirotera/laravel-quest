3b244073aabee986d106494a602598b5dce9380b
EC2 Default User <ec2-user@ip-172-31-17-158.us-east-2.compute.internal>
2020-12-22 13:31:31 +0000

    API

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('movies', 'MoviesController', ['only' => ['create', 'store', 'destroy']]);
 });
 
+Route::resource('rest','RestappController', ['only' => ['index', 'show', 'create', 'store', 'destroy']]); //追記
+
+
